442719a11fba5f57e8bc2e2f31a2076612bf044c
Nubiza <104552412+Nubiza@users.noreply.github.com>
2023-03-18 21:26:27 +0700

    add proxy update

---
+++ update.sh
@@ -0,0 +1,18 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${GREEN}Updating proxy...${ENDCOLOR}"
+clear
+sleep 1
+if [ -f "proxy_linux" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm proxy_linux
+    sleep 1
+fi
+echo -e "${GREEN}Getting new proxy...${ENDCOLOR}"
+wget https://github.com/Nubiza/proxy/raw/main/proxy_linux
+sleep 1
+echo -e "${GREEN}Executing proxy...${ENDCOLOR}"
+chmod +x proxy_linux
+./proxy_linux
