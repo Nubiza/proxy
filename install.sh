5403c8222682a0dc5e07b2994f61073c591b457b
Nubiza <104552412+Nubiza@users.noreply.github.com>
2023-03-19 09:39:11 +0700

    Add change permission

--- install.sh
+++ install.sh
@@ -9,9 +9,10 @@ if [ -f "proxy_linux" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
     rm proxy_linux
     sleep 1
-    echo -e "${GREEN}Installing proxy...${ENDCOLOR}"
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
 wget -q https://github.com/Nubiza/proxy/raw/main/proxy_linux
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
 echo -e "${GREEN}Execute proxy with this command: ./proxy_linux${ENDCOLOR}"
+chmod +x proxy_linux
