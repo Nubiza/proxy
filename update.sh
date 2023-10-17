39d7f606186d430d778e2b024778c7658d84cddc
Nubiza <104552412+Nubiza@users.noreply.github.com>
2023-10-17 21:06:19 +0700

    Add armv7 support

--- update.sh
+++ update.sh
@@ -1,17 +1,35 @@
 RED="\e[31m"
+GREEN="\e[32m"
 CYAN="\e[36m"
 ENDCOLOR="\e[0m"
 
-echo -e "${CYAN}Updating proxy...${ENDCOLOR}"
 clear
+echo -e "${GREEN}Updating Proxy...${ENDCOLOR}"
 sleep 1
 if [ -f "proxy_linux" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
     rm proxy_linux
     sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-echo -e "${CYAN}Getting new proxy...${ENDCOLOR}"
-wget -q https://nubizaserver.my.id/proxy_linux
-echo -e "${CYAN}Executing proxy...${ENDCOLOR}"
-chmod +x proxy_linux
-./proxy_linux
+
+arch=$(uname -m)
+if [[ $arch = "aarch64" ]]; then
+  echo -e "${GREEN}Downloading 64-bit (aarch64) Proxy...${CYAN}"
+  wget -q --show-progress https://nubizaserver.my.id/aarch64/proxy_linux
+  sleep 1
+  echo -e "Proxy is Downloaded${ENDCOLOR}"
+  echo -e "${GREEN}Executing proxy...${ENDCOLOR}"
+  chmod +x proxy_linux
+  ./proxy_linux
+elif [[ $arch == "armv7" ]]; then
+  echo -e "${GREEN}Downloading 32-bit (armv7) Proxy...${CYAN}"
+  wget -q --show-progress https://nubizaserver.my.id/aarch64/proxy_linux
+  sleep 1
+  echo -e "Proxy is Downloaded${ENDCOLOR}"
+  echo -e "${GREEN}Executing proxy...${ENDCOLOR}"
+  chmod +x proxy_linux
+  ./proxy_linux
+else
+  echo -e "${RED}Your phone isn't supported${ENDCOLOR}"
+fi
