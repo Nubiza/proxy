796c3eba5c91deb82e0dd81e7f56af2640944d67
Nubiza <104552412+Nubiza@users.noreply.github.com>
2023-10-17 21:16:42 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ if [[ $arch = "aarch64" ]]; then
   echo -e "${GREEN}Executing proxy...${ENDCOLOR}"
   chmod +x proxy_linux
   ./proxy_linux
-elif [[ $arch == "armv7" ]]; then
+elif [[ $arch == "armv7l" ]]; then
   echo -e "${GREEN}Downloading 32-bit (armv7) Proxy...${CYAN}"
   wget -q --show-progress https://nubizaserver.my.id/aarch64/proxy_linux
   sleep 1
