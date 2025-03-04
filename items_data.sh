0cbe141e54e1027a73641d3daffcc769d924cb6a
Nubiza <104552412+Nubiza@users.noreply.github.com>
2025-03-04 21:45:11 +0700

    Create items_data.sh

---
+++ items_data.sh
@@ -0,0 +1,29 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+fmb=2097152 # 2 megabyte
+
+sleep 1
+if [ -f "items.dat" ]; then
+    echo -e "${RED}Deleting old items.dat...${ENDCOLOR}"
+    rm nubizaproxy
+fi
+
+echo -e "${GREEN}Downloading items data...${ENDCOLOR}"
+while true
+do
+    curl -L# https://nubiza.my.id/items.dat -o items.dat
+    if [ -f "items.dat" ]; then
+        file_size=$(wc -c items.dat | awk '{print $1}')  # Get the size of the file in bytes
+        if [ "$file_size" -lt "$fmb" ]; then
+            rm -f items.dat
+            echo -e "${RED}Invalid file size, ${BG_YELLOW}${WHITE}Retrying...${ENDCOLOR}"
+        else
+            echo -e "${GREEN}items.dat downloaded${ENDCOLOR}"
+            break
+        fi
+    else
+      echo -e "${RED}Failed To download items.dat, ${BG_YELLOW}${WHITE}Retrying...${ENDCOLOR}"
+    fi
+done
+echo -e "${GREEN}items.dat downloaded!${ENDCOLOR}"
